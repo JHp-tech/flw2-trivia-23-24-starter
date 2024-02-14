bc32f90c5b8ffc5d330385fb6c3393216773d3ea
JHp-tech <105798553+JHp-tech@users.noreply.github.com>
2024-02-14 23:05:35 +0000

    Trivia progress
    Progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
-import './App.css'
-import data from "./sample_data.json";
-
+import Question from "./components/Question";
 export default function App() {
+  const currentQuestionNumber = 0;
   return (
     <div className="app">
       <h1 className="title">Trivia!</h1>
+      <Question question={data[currentQuestionNumber].text} />
     </div>
   );
 }
